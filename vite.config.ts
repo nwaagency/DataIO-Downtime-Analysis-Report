6b1e0b63d39f1128aa4ae63cd19be16623f911dd
nwaagency <nwa.agency.josh@gmail.com>
2026-03-15 04:05:47 +0000

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base:"/SC-Engineering-Website-Review",
+    base: "/SC-Engineering-Website-Review/",
   server: {
     host: "::",
     port: 8080,
